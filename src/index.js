445df5a6d57fb8c22c3afe92bf862526b6414cb7
ShilpiMaurya <meshilpi94@gmail.com>
2019-08-27 18:28:18 +0900

    get request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      imgObj: null
+    };
+  }
+  onLoadMorePics = () => {
+    this.setState({ loading: true });
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        console.log(data[0].url);
+        const newState = {
+          loading: false,
+          imgObj: data[0]
+        };
+        this.setState(newState);
+      });
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentWillMount() {
+    this.onLoadMorePics();
+  }
+  onMarkAsFavorite = () => {
+    fetch("https://api.thecatapi.com/v1/favourites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "85eb2c59-14e5-4281-ba81-ae03cde1d50a"
+      },
+      body: JSON.stringify({
+        image_id: this.state.imgObj.id
+      })
+    }).then(response => response.json());
+  };
+
+  render() {
+    if (this.state.loading === true) {
+      return <h1>Loading.....</h1>;
+    } else {
+      return (
+        <div>
+          <h1>CAT'S WORLD</h1>
+          <div>
+            <button onClick={this.onLoadMorePics}>Load More Pics</button>
+            <button onClick={this.onMarkAsFavorite}>Mark as Favorite</button>
+            
+          </div>
+          <img
+            src={this.state.imgObj.url}
+            alt="catimage"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%"
+            }}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
