5bdd5542477e139d4e681c70e45ca17a4e9286a8
ShilpiMaurya <meshilpi94@gmail.com>
2019-09-09 23:40:17 +0900

    Updates

---
+++ precache-manifest.80ca2341823c649f1996e7e56a669fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d684d0d70ab53a14fe71d987bb3d799",
+    "url": "/cat-app/index.html"
+  },
+  {
+    "revision": "05a20a74670fa5fb70e5",
+    "url": "/cat-app/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "62904ed668ac2aee3857",
+    "url": "/cat-app/static/css/main.56501265.chunk.css"
+  },
+  {
+    "revision": "05a20a74670fa5fb70e5",
+    "url": "/cat-app/static/js/2.a6ddd1e5.chunk.js"
+  },
+  {
+    "revision": "62904ed668ac2aee3857",
+    "url": "/cat-app/static/js/main.f2ba0c8e.chunk.js"
+  },
+  {
+    "revision": "1cab2b004f108b988900",
+    "url": "/cat-app/static/js/runtime~main.57ecd5cf.js"
+  }
+]);
